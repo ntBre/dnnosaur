bf55b7dc39457098c8817c2d2bd808f9b190614a
Brent Westbrook <brentrwestbrook@gmail.com>
2023-05-02 15:51:19 -0500

    add profile recipe and replace while loops with for

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
 clippy:
 	cargo clippy --all
+
+
+build:
+	RUSTFLAGS='-g' cargo build --release
+
+profile: build
+	valgrind --tool=callgrind --callgrind-out-file=callgrind.out		\
+        --collect-jumps=yes --simulate-cache=yes target/release/dnnosaur
--- src/layer.rs
+++ src/layer.rs
@@ -40,20 +40,14 @@ where
     pub fn forward(&mut self, inputs: Vec<f64>) -> Vec<f64> {
         let batch_size = inputs.len() / I;
         let mut outputs = vec![0.0; batch_size * O];
-        let mut b = 0;
-        while b < batch_size {
-            let mut o = 0;
-            while o < O {
+        for b in 0..batch_size {
+            for o in 0..O {
                 let mut sum = 0.0;
-                let mut i = 0;
-                while i < I {
+                for i in 0..I {
                     sum += inputs[b * I + i] * self.weights[O * i + o];
-                    i += 1;
                 }
                 outputs[b * O + o] = sum;
-                o += 1;
             }
-            b += 1;
         }
         self.last_inputs = inputs;
         outputs
@@ -65,22 +59,16 @@ where
         let batch_size = self.last_inputs.len() / I;
         let mut input_grads = vec![0.0; batch_size * I];
 
-        let mut b = 0;
-        while b < batch_size {
-            let mut i = 0;
-            while i < I {
-                let mut o = 0;
-                while o < O {
+        for b in 0..batch_size {
+            for i in 0..I {
+                for o in 0..O {
                     weight_grads[i * O + o] += (grads[b * O + o]
                         * self.last_inputs[b * I + i])
                         / batch_size as f64;
                     input_grads[b * I + i] +=
                         grads[b * O + o] * self.weights[i * O + o];
-                    o += 1;
                 }
-                i += 1;
             }
-            b += 1;
         }
         LayerGrads::new(weight_grads, input_grads)
     }
