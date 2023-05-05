7c10e207cb89b5ce73f3a96bedf664c4cf0a3fe9
Brent Westbrook <brentrwestbrook@gmail.com>
2023-05-05 14:52:17 -0500

    train longer in main

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use dnnosaur::{qff::Qff, Train};
 
 fn main() {
     // mnist::Data::read_mnist().train(25);
-    Qff::default().load("qff_data").unwrap().train(50);
+    Qff::default().load("qff_data").unwrap().train(200);
 }
